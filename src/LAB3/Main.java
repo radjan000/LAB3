d96451741e4ee61571a6729bca585884370f9efe
radjan000 <80147496+radjan000@users.noreply.github.com>
2021-04-10 12:28:27 +0200

    LAB3

--- src/LAB3/Main.java
+++ src/LAB3/Main.java
@@ -1,4 +1,28 @@
 package LAB3;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class Main {
+    
+    public static void main(String[] args) {
+
+        ArrayList<Task> list = new ArrayList<>();
+
+        Task task1 = new Task("Zad1", "Bla1", Task.Category.NEW, LocalDateTime.of(2021, 3, 5, 12, 1));
+        Task task2 = new Task("Zad2", "Bla2", Task.Category.IN_PROGRESS, LocalDateTime.of(2020, 1, 2, 23, 59));
+        Task task3 = new Task("Zad3", "Bla3", Task.Category.DONE, LocalDateTime.now());
+        Task task4 = new Task("Zad4", "Bla4", Task.Category.NEW, LocalDateTime.now());
+
+        task4.setStop(task4.getStop().plusMonths(3));
+
+        list.add(task1);
+        list.add(task2);
+        list.add(task3);
+        list.add(task4);
+
+        for (Task task : list) {
+            System.out.println(task);
+        }
+    }
 }
---
+++ src/LAB3/Task.java
@@ -0,0 +1,65 @@
+package LAB3;
+
+import java.time.LocalDateTime;
+
+public class Task {
+
+    public enum Category {
+        NEW, IN_PROGRESS, DONE
+    }
+
+         String taskName;
+         String taskDescription;
+         Category taskCategory;
+         LocalDateTime taskDateTimeStart;
+         LocalDateTime taskDateTimeStop;
+
+        public Task(String taskName, String taskDescription, Category taskCategory,
+                    LocalDateTime taskDateTimeStop) {
+        this.taskName = taskName;
+        this.taskDescription = taskDescription;
+        this.taskCategory = taskCategory;
+        this.taskDateTimeStop = taskDateTimeStop;
+        this.taskDateTimeStart = LocalDateTime.now();
+    }
+
+    public String getName() {
+        return taskName;
+    }
+    public void setName(String newName) {
+        taskName = newName;
+    }
+    public String getDescription() {
+        return taskDescription;
+    }
+    public void setDescription(String newDescription) {
+        taskDescription = newDescription;
+    }
+    public Category getCategory() {
+        return taskCategory;
+    }
+    public void setCategory(Category newCategory) {
+        taskCategory = newCategory;
+    }
+    public LocalDateTime getStart() {
+        return taskDateTimeStart;
+    }
+    public void setStart(LocalDateTime newStart) {
+        taskDateTimeStart = newStart;
+    }
+    public LocalDateTime getStop() {
+        return taskDateTimeStop;
+    }
+    public void setStop(LocalDateTime newStop) {
+        taskDateTimeStop = newStop;
+    }
+    public String toString() {
+        return "Name: " + taskName + ", description: " + taskDescription +
+                ", category: " + taskCategory +
+                "\nstart time: " + taskDateTimeStart +
+                "\nend time: " + taskDateTimeStop;
+    }
+}
+
+
+
